bd91f959a5ad9159e7dce0076ef2cbfec1ab2380
Nick Hodges <nickhodges@gmail.com>
2024-08-03 17:59:35 -0400

    Collection Config File

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+
+// Define a `type` and `schema` for each collection
+const blogCollection = defineCollection({
+    type: 'content',
+    schema: z.object({
+      title: z.string(),
+      pubDate: z.date().transform((val) => new Date(val)),
+      description: z.string().min(10).max(150),
+      author: z.string().optional(),
+    })
+});
+
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  blog: blogCollection,
+};
