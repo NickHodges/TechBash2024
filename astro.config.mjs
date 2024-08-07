931b1649848ef720d5350363c9e658c5a1651a02
Nick Hodges <nickhodges@gmail.com>
2024-08-07 05:48:47 -0400

    Set config for actions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,10 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'hybrid',
   site: 'http://localhost:4321',
    integrations: [sitemap()],
+   experimental: {
+    actions: true,
+  },
 });
