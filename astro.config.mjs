ecf38e82f31bc681027bf557ac510c75d310c4af
Nick Hodges <nickhodges@gmail.com>
2024-08-25 17:01:34 -0400

    Add auth.js for Astro plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,15 @@ import sitemap from '@astrojs/sitemap';
 import react from '@astrojs/react';
 import node from "@astrojs/node";
 import db from "@astrojs/db";
-
 import mdx from "@astrojs/mdx";
+
+import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'http://localhost:4321',
-  integrations: [sitemap(), db(), react(), mdx()],
+  integrations: [sitemap(), db(), react(), mdx(), auth()],
   experimental: {
     actions: true,
     serverIslands: true
