211439bc4c8390e286ace9bc7b91f40107a3ac9a
Nick Hodges <nickhodges@gmail.com>
2024-09-01 15:00:09 -0400

    install astroDB

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import db from "@astrojs/db";
-
 import node from "@astrojs/node";
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'hybrid',
   site: 'https://127.0.0.1:4321',
-  integrations: [sitemap()],
+  integrations: [sitemap(), db()],
   experimental: {
     actions: true
   },
